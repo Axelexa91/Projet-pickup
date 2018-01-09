bd0306e1f8d9b4000c04d1f384267a0568b42583
Axelexa91 <35218886+Axelexa91@users.noreply.github.com>
2018-01-09 10:24:11 +0100

    Add files via upload

---
+++ Define.h
@@ -0,0 +1,24 @@
+
+#define OK 1
+#define ERROR -1
+
+#define MAX_ID_CLIENT 9999 
+#define MAX_NOM_CLIENT 21
+#define MAX_PRENOM_CLIENT 21
+#define MAX_LOCALISATION_CLIENT 50
+#define 
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+
---
+++ Structures.h
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*--------------------------------------------------------------------*/
+/*                          DONNEES STOCKEES                          */
+/*--------------------------------------------------------------------*/
+
+/*La structure qui défini un client (UNE LIGNE) */ 
+typedef struct Client {	
+	int ID;								//4 chiffres pour l'ID
+	char Nom[21];						//Nom de 20 caractère alpha ( +1 pour le \0 de fin de chaine de caractère )
+	char Prenom[21];					//Prenom de 20 caractère alpha ( +1 pour \0 de fin de chaine de caractère)
+	char Localisation[51];				//localisation ( adresse ) du client, 50 caractères ( +1 pour \0 de fin de chainede caractère )
+	char Numero[11];					//numéro de téléphone, 10 chiffres dit caractère numérique ( et PAS alphanumérique ) (+1 pour le \0 de fin de chaine de caractère )
+	char MotDePasse[5];					//Mot de passe de 4 caractère alphanumérique (+1 pour le \0à de fin de chaine de caractère)
+} Client;
+/*La structure qui regroupe tous les clients (UN FICHIER) */
+typedef struct ClientFile {
+	Client *TableauDeClient;			//Un pointeur sur un tableau de Client (Nécéssite un malloc et des realloc)
+	int NombreDeClients;				//Nombre de client enregistrer pour le moment
+
+} ClientFile;
+
+
+
+
+
+/*La structure qui definit un Article (UNE LIGNE) */
+typedef struct Article {
+	int ID;								//4 chiffres pour l'ID
+	char Nom[21];						//Nom de l'article de 20 caractère ( +1 pour le \0 de fin de chaine de caractère )
+	char Description[21];				//Description de l'article de 20 caractère de long ( +1 pour le \0 de fin de chaine de caractère )
+	char Type[21];						//Type de l'article de 20 caractère de long
+} Article;
+/*La structure qui regroupe tous les articles défini (UN FICHIER) */
+typedef struct ArticleFile {	
+	Article *TableauDesArticles;		//Un pointeur sur un tableau d'article (Necessite un malloc et des realloc)
+	int NombreArticles;					//Nombre d'article dans ce fichier d'article
+} ArticleFile;
+
+
+
+
+/*La structure qui defini une commande (UNE LIGNE) */
+typedef struct Commande {
+	int ClientID;						//L'ID du client qui passe la commande
+	int ArticleID;						//L'ID de l'article que le client a commander
+	int Quantity;						//La quantité de c et article que le client à commander
+	int CodeRetrait;					//Le code que le client doit taper afin de retirer son colis dans les pickup Station
+} Commande;
+/*La structure qui regroupe toutes les commandes (UN FICHIER) */
+typedef struct CommandeFile {
+	Commande *TableauDesCommandes;		//Un pointeur qui pointe sur un tableau de toutes les commandes
+	int NombreDeCommande;				//Le nombre de commandes dans ce fichier de commande
+} CommandeFile;
+
+
+
+
+/*strucutre qui défini les stocks d'un article dans un entrepot (UNE LIGNE) */
+typedef struct ArticleStocks {
+	int EntrepotID;					//ID de l'entrepot (USELESS)
+	int ArticleID;					//ID de l'article
+	int Quantity;					//Quantité en stock de cet article
+	int Hauteur;					//Hauteur de l'article	
+	int Largeur;					//Largeur de l'article
+	int Profondeur;					//Profondeur de l'article
+} ArticleStocks;
+/*structure qui defini tous les article stockées dans un entrepot (UN FICHIER) */
+typedef struct StocksEntrepot {
+	ArticleStocks *TabArticleStock;	//Un pointeur qui pointe sur un tableau qui liste tous les articles en stocks dans CET entrepot
+	int NombreArticle;				//Le nombre d'article DIFFERENT en stock
+} StocksEntrepot;
+
+/*structure qui defini un entrepot (le dossier ou il faut aller voir son stock) (UNE LIGNE) */
+typedef struct Entrepot {
+	int EntrepotID;					//ID de l'entrepot
+	char NomDuDossier[16];			//Nom du dossier dans lequel se trouve le dossier stock de cet entrepot
+} Entrepot;
+/*structure qui contient tous les emplacements des dossier des entrepots (UN FICHIER) */
+typedef struct EntrepotFile {
+	Entrepot *TabEntrepot;			//Un poiteur qui pointe sur un tableau listant chaquun des entrepots
+	int NombreEntrepot;				//Nombre d'entrepot a disposition
+} EntrepotFile;
+
+
+
+
+
+/*structure qui defini une station (UNE LIGNE) */
+typedef struct Station {
+	int PickupStationID;		//L'ID de la pickup station
+	int SlotID;					//L'ID du slot de cette pickup station
+	int Hauteur;				//La hauteur de ce slot
+	int Largeur;				//La largeur de ce slot
+	int Profondeur;				//La profondeur de ce slot
+	int Disponible;				//Si slot disponible mettre a 1, sinon 0
+} Station;
+/*structure qui regroupe toutes nos stations*/
+typedef struct StationFile {
+	Station *TableauDesStation;	//pointeur qui pointe vers un tableau qui liste les stations a disposition
+	int NombreDeStation;		//Le Nombre de station a disposition
+} StationFile;
+
+
+
+
+
+/*structure achat*/
+typedef struct Achat {
+	int AchatID;				//L'ID de l'achat... ? Pas compris c'etait pendant la pause y avais plus personne pour me repondre... :(
+} Achat;
+
+
+
+
+
+/*structure dispatching*/
+typedef struct Dispatching {
+	int DispatchingID;			//L'id du dispatching... Pareil :'(
+} Dispatching;
+
+
+/*--------------------------------------------------------------------*/
+/*                          DONNEES ENVOYEES                          */
+/*--------------------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
