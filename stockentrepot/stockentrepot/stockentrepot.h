446665f88f2e44d72c4d9a753663ec26c08b2c67
astreides <35225989+astreides@users.noreply.github.com>
2018-01-11 16:35:21 +0100

    lien fait

--- stockentrepot/stockentrepot/stockentrepot.h
+++ stockentrepot/stockentrepot/stockentrepot.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include "Define.h"
 #include "Structures.h"
+#include <string.h>
+#include <time.h>
 int compterligne(const char * fichierlu);
 float pourcentagecommandefait(Commande *demande, ArticleStocks *stockentrepot, int indiceentrepot, int (*quantitémanquante)[6], const char *fichier, const char *comandealias);
 int fonction(const char *fichier, const char *comandealias, const char *fichierenvoitalias);
